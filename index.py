b266ff206bf60570710bd34f7630e620e9bf1dfe
Ren Saito <ren.saito@scarlatti.co.nz>
2024-04-07 17:44:10 +1200

    - added Google Quickdraw dataset - added quickdraw python package - added functionality for skirrblio-bot to draw chosen word

--- index.py
+++ index.py
@@ -6,6 +6,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.webdriver import WebDriver
 import random
 from math import floor
+from quickdraw import QuickDrawData
+
+
+qd = QuickDrawData()
+
 
 def run(invite_link):
     with webdriver.Firefox() as driver:
@@ -16,18 +21,24 @@ def run(invite_link):
             )
         )
         print("Joined Game, waiting for host to start game...")
-        input("Started game?")
+        WebDriverWait(driver, 60).until(
+            EC.text_to_be_present_in_element(
+                (By.CSS_SELECTOR, "#game-canvas > .overlay-content > .text.show"),
+                "Choose a word",
+            )
+        )
+        print("Game started!")
         game_state = get_game_state(driver)
         match game_state:
             case "Choose a word":
                 # Buddy is choosing a word and drawing
-                choose_word(driver)
+                word = choose_word(driver)
+                draw_word(driver, word)
                 pass
             case _:
                 # Buddy is guessing the word
                 pass
-        
-        input('end')
+        input("end")
         pass
     pass
 
@@ -44,23 +55,58 @@ def join_game(driver: WebDriver, invite_link: str):
 
 def wait_for_game_start(driver: WebDriver):
     WebDriverWait(driver, 30).until(
-		EC.presence_of_element_located((By.CSS_SELECTOR, "#game-word .description.waiting"))
-	)
+        EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#game-word .description.waiting")
+        )
+    )
     pass
 
 
 def get_game_state(driver: WebDriver) -> str:
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".text.show")))
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".text.show"))
+    )
     elem_state = driver.find_element(By.CSS_SELECTOR, ".text.show")
     return elem_state.text
 
 
-def choose_word(driver: WebDriver):
+def choose_word(driver: WebDriver) -> str:
     words = driver.find_elements(by=By.CSS_SELECTOR, value=".words.show > .word")
     assert len(words) == 3
-    random_index = (random.random() * 3)
+    random_index = random.random() * 3
     chosen_word = words[floor(random_index)]
     chosen_word.click()
+    return chosen_word.text
+
+
+def draw_word(driver: WebDriver, word: str):
+    print("draw word", word)
+    assert word in qd.drawing_names
+    WebDriverWait(driver, 60).until_not(
+        EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#game-canvas > .overlay.show")
+        )
+    )
+    canvas = driver.find_element(by=By.CSS_SELECTOR, value="#game-canvas > canvas")
+    width = canvas.size["width"]
+    height = canvas.size["height"]
+    drawing = qd.get_drawing(word)
+    for stroke in drawing.strokes:
+        action = webdriver.ActionChains(driver).move_to_element_with_offset(canvas, -width // 2 + 50, - height // 2 + 50)
+        initial_coord = stroke[0]
+        action.move_by_offset(initial_coord[0], initial_coord[1])
+        action.click_and_hold()
+        for i in range(1, len(stroke)):
+            prev_coord = stroke[i - 1]
+            coord = stroke[i]
+            x_offset = coord[0] - prev_coord[0]
+            y_offset = coord[1] - prev_coord[1]
+            action.move_by_offset(x_offset, y_offset)
+            pass
+        action.release()
+        action.perform()
+        pass
+    print("drawing complete")
     pass
 
 
